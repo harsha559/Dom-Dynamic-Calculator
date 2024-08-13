9526ab00ed933f894acda9b4834c28093fdd93fc
harsha559 <harshavardhanch62@gmail.com>
2024-08-13 19:30:35 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+let result = document.querySelector("#result");
+
+function Solve(val) {
+   //  var v = document.getElementById('result');
+    result.value += val;
+}
+
+function Result() {
+   // var num1 = document.getElementById('result').value;
+
+      var num2 = eval(result.value.replace('x', '*'));
+      result.value = num2;
+   
+}
+
+function Clear() {
+   //  var inp = document.getElementById('result');
+    result.value = '';
+}
+
+function Back() {
+   //  var ev = document.getElementById('result');
+    result.value = result.value.slice(0, -1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
